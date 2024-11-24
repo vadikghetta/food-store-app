91fe1fe609fa8e9c1364dc7621424879acadec6e
vadikghetta <helloiamvadik@mail.ru>
2024-11-24 16:35:36 +0300

    add layout

---
+++ src/layout/layout/layout.tsx
@@ -0,0 +1,38 @@
+import { NavLink, Outlet } from 'react-router';
+import styles from './styles.module.css';
+import Button from '../../ui/button/button';
+import clsx from 'clsx';
+
+
+
+export function Layout() {
+
+	return <div className={styles['layout']}>
+		<div className={styles['sidebar']}>
+			<div className={styles['user']}>
+				<img className={styles['avatar']} src="/avatar.png" alt="Аватар пользователя" />
+				<div className={styles['name']}>{'Hello'}</div>
+				<div className={styles['email']}>{'hello@gmail.com'}</div>
+			</div>
+			<div className={styles['menu']}>
+				<NavLink to='/' className={({ isActive }) => clsx(styles['link'], {
+					[styles.active]: isActive
+				})}>
+					<img src="/menu-icon.svg" alt="Иконка меню" />
+					Меню</NavLink>
+				<NavLink to='/cart' className={({ isActive }) => clsx(styles['link'], {
+					[styles.active]: isActive
+				})}>
+					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина <span className={styles['cart-count']}>10</span></NavLink>
+
+			</div>
+			<Button className={styles['exit']} >
+				<img src="/exit-icon.svg" alt="Иконка выхода" />
+				Выход
+			</Button>
+		</div>
+		<div className={styles['content']}>
+			<Outlet />
+		</div>
+	</div>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 import { Menu } from './pages/menu/Menu';
 import { Cart } from './pages/cart/Cart';
 import { ErrorPage } from './pages/error/error';
+import { Layout } from './layout/layout/layout';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Menu />
-	},
-	{
-		path: '/cart',
-		element: <Cart />
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			}
+		]
 	},
 	{
 		path: '*',
