ee385d89ba8601e4ff83242838c1097009de57d1
vadikghetta <helloiamvadik@mail.ru>
2024-11-24 16:39:35 +0300

    add heading

---
+++ src/ui/heading/heading.tsx
@@ -0,0 +1,11 @@
+import styles from './Headling.module.css';
+import clsx from 'clsx';
+import { HeadlingProps } from './heading.props';
+
+function Headling({ children, className, ...props }: HeadlingProps) {
+	return (
+		<h1 className={clsx(className, styles['h1'])} {...props}>{children}</h1>
+	);
+}
+
+export default Headling;
