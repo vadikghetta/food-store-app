aafad10ba665f8d2a818a28185524229ae53329d
vadikghetta <helloiamvadik@mail.ru>
2024-11-24 15:50:45 +0300

    add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { Menu } from './pages/menu/Menu';
+import { Cart } from './pages/cart/Cart';
+import { ErrorPage } from './pages/error/error';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Menu />
+	},
+	{
+		path: '/cart',
+		element: <Cart />
+	},
+	{
+		path: '*',
+		element: <ErrorPage />
+	}
+]);
+
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
